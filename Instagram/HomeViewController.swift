b148a8237c0fed1d11ecacd18e830c462c0340c5
yosuke aga <yosuke0607@aga-no-macbook-pro.local>
2016-03-23 15:28:31 +0900

    課題

--- Instagram/HomeViewController.swift
+++ Instagram/HomeViewController.swift
@@ -7,17 +7,125 @@
 //
 
 import UIKit
+import Firebase
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDataSource , UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var firebaseRef: Firebase!
+    var postArray: [PostData] = []
+    
+     //DataSourceプロトコル
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postArray.count
+    }
+     //DataSourceプロトコル
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell" , forIndexPath: indexPath) as! PostTableViewCell
+        cell.postData = postArray[indexPath.row]
+        
+        cell.likeButton.addTarget(self, action:"handleButton:event:", forControlEvents:  UIControlEvents.TouchUpInside)
+        
+        cell.layoutIfNeeded()
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        // Auto Layoutを使ってセルの高さを動的に変更する
+        return UITableViewAutomaticDimension
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // セルをタップされたら何もせずに選択状態を解除する
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    func handleButton(sender: UIButton, event:UIEvent) {
+        
+        // タップされたセルのインデックスを求める
+        let touch = event.allTouches()?.first
+        let point = touch!.locationInView(self.tableView)
+        let indexPath = tableView.indexPathForRowAtPoint(point)
+        
+        // 配列からタップされたインデックスのデータを取り出す
+        let postData = postArray[indexPath!.row]
+        
+        // Firebaseに保存するデータの準備
+        let uid = firebaseRef.authData.uid
+        
+        if postData.isLiked {
+            // すでにいいねをしていた場合はいいねを解除するためIDを取り除く
+            var index = -1
+            for likeId in postData.likes {
+                if likeId == uid {
+                    // 削除するためにインデックスを保持しておく
+                    index = postData.likes.indexOf(likeId)!
+                    break
+                }
+            }
+            postData.likes.removeAtIndex(index)
+        } else {
+            postData.likes.append(uid)
+        }
+        
+        let imageString = postData.imageString
+        let name = postData.name
+        let caption = postData.caption
+        let time = (postData.date?.timeIntervalSinceReferenceDate)! as NSTimeInterval
+        let likes = postData.likes
+        
+        // 辞書を作成してFirebaseに保存する
+        let post = ["caption": caption!, "image": imageString!, "name": name!, "time": time, "likes": likes]
+        let postRef = Firebase(url: CommonConst.FirebaseURL).childByAppendingPath(CommonConst.PostPATH)
+        postRef.childByAppendingPath(postData.id).setValue(post)
+    }
+    
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+       
+        print(postArray.count)
+        
+        let nib = UINib(nibName: "PostTableViewCell", bundle: nil)
+        tableView.registerNib(nib, forCellReuseIdentifier: "Cell")
+        tableView.rowHeight = UITableViewAutomaticDimension
+        
+        firebaseRef = Firebase(url: CommonConst.FirebaseURL)
+        
+        firebaseRef.childByAppendingPath(CommonConst.PostPATH).observeEventType(FEventType.ChildAdded, withBlock: { snapshot in
+            
+            let postData = PostData(snapshot: snapshot, myId: self.firebaseRef.authData.uid)
+            self.postArray.insert(postData, atIndex: 0)
+        
+            self.tableView.reloadData()
+        })
+        
+        firebaseRef.childByAppendingPath(CommonConst.PostPATH).observeEventType(FEventType.ChildChanged, withBlock: { snapshot in
+         
+            let postData = PostData(snapshot: snapshot, myId: self.firebaseRef.authData.uid)
+            
+            var index: Int = 0
+            for post in self.postArray {
+            
+                if post.id == postData.id{
+                
+                    index = self.postArray.indexOf(post)!
+                    break
+                }
+            }
+            
+            self.postArray.removeAtIndex(index)
+            self.postArray.insert(postData, atIndex: index)
+            
+            let indexPath = NSIndexPath(forRow: index, inSection: 0)
+            self.tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.None)
+        })
+    
     }
 
     override func didReceiveMemoryWarning() {
